e57461a4b44767a1f0406e601149e664d2405cdc
Meni Samet <meni432@gmail.com>
2024-06-22 20:56:36 +0000

    feat: Add lambda functions for API Gateway example

---
+++ cdk/api-gateway-example/another-lambda/another.js
@@ -0,0 +1,15 @@
+exports.handler = async (event, context) => {
+    try {
+        // Your code logic goes here
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Hello, world! from Another' })
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' })
+        };
+    }
+};
